96ff7a09ed63167d5fb6bf3d02f50c13fbb7d413
dobarrofranco <mr.francodobarro@gmail.com>
2023-09-20 12:21:47 -0300

    02-sql

--- 02-sql/homework/01 - Exercises/homework.js
+++ 02-sql/homework/01 - Exercises/homework.js
@@ -1,14 +1,14 @@
 /* ¡Escribe tus comandos en este archivo! */
 
-const ejercicio02 = '';
+const ejercicio02 = 'SELECT * FROM movies WHERE duration < 90;';
 
-const ejercicio03 = '';
+const ejercicio03 = 'SELECT * FROM movies WHERE year > 1930 AND year < 1940;';
 
-const ejercicio04 = '';
+const ejercicio04 = 'SELECT * FROM movies WHERE tittle LIKE "%til%";';
 
-const ejercicio05 = '';
+const ejercicio05 = 'SELECT * FROM movies WHERE array_length(actors, 1) = 1;';
 
-const ejercicio06 = '';
+const ejercicio06 = 'SELECT title, ROUND(AVG(rating)) FROM movies, unnest (ratings) as rating GROUP BY title ORDER BY round DESC;';
 
 const ejercicio07 = '';
 
